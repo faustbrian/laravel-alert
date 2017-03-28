c0a3e337f24aa1a96d4c11c0f0cfa5430709e88e
Brian Faust <faustbrian@users.noreply.github.com>
2017-03-28 14:25:16 +0300

    Add css classes to config

--- resources/config/alert.php
+++ resources/config/alert.php
@@ -13,4 +13,11 @@ return [
 
     'template' => 'alert::templates.bootstrap.default',
 
+    'classes' => [
+        'success' => 'success',
+        'info' => 'info',
+        'warning' => 'warning',
+        'error' => 'danger',
+    ]
+
 ];
